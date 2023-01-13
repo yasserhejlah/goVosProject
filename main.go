564a80ffd8fbb19e052dfb67b374cb6ea488cda8
Your Name <you@example.com>
2023-01-14 02:14:49 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+import (
+ "github.com/yasserhejlah/goVosProject/database"
+ "github.com/yasserhejlah/goVosProject/router"
+ "github.com/gofiber/fiber/v2"
+ "github.com/gofiber/fiber/v2/middleware/cors"
+ "github.com/gofiber/fiber/v2/middleware/logger"
+ _ "github.com/lib/pq"
+)
+func main() {
+ database.Connect()
+ app := fiber.New()
+ app.Use(logger.New())
+ app.Use(cors.New())
+ router.SetupRoutes(app)
+ // handle unavailable route
+ app.Use(func(c *fiber.Ctx) error {
+  return c.SendStatus(404) // => 404 "Not Found"
+ })
+ app.Listen(":8080")
+}

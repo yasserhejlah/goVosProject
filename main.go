237bf8813e3c559400f12b602a7b664fe542366d
unknown <developer@mapletele.com>
2023-01-14 14:27:35 +0300

    new Update

--- main.go
+++ main.go
@@ -1,21 +1,20 @@
 package main
+
 import (
- "github.com/yasserhejlah/goVosProject/database"
- "github.com/yasserhejlah/goVosProject/router"
- "github.com/gofiber/fiber/v2"
- "github.com/gofiber/fiber/v2/middleware/cors"
- "github.com/gofiber/fiber/v2/middleware/logger"
- _ "github.com/lib/pq"
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
+	"github.com/yasserhejlah/goVosProject/database"
 )
+
 func main() {
- database.Connect()
- app := fiber.New()
- app.Use(logger.New())
- app.Use(cors.New())
- router.SetupRoutes(app)
- // handle unavailable route
- app.Use(func(c *fiber.Ctx) error {
-  return c.SendStatus(404) // => 404 "Not Found"
- })
- app.Listen(":8080")
+	database.Connect()
+	app := fiber.New()
+	/* app.Use(logger.New())
+	app.Use(cors.New())
+	router.SetupRoutes(app)
+	// handle unavailable route
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404) // => 404 "Not Found"
+	}) */
+	app.Listen(":8080")
 }
